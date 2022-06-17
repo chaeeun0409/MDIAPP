26f1bd85d74f56f40b4704d3e6312cc46d4ef37a
chaeeun0409 <s2017@e-mirim.hs.kr>
2022-06-17 09:58:55 +0900

    초기 커밋.

---
+++ MDIAPP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDIAPP
+{
+    public static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        private static void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel, MessageBoxIcon.);
+            }while (result == DialogResult.Retry);
+        }
+    }
+}
